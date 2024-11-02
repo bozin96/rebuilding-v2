dd9dbc0e0f4033a23e763bcbb1ab9c3cfc5bf2d1
nikolabozinovic1996@yahoo.com <nikolabozinovic1996@yahoo.com>
2024-11-02 17:36:27 +0100

    fix(redirecton): trying to remove index.html from path

--- js/catalogs.js
+++ js/catalogs.js
@@ -36,25 +36,25 @@ document.addEventListener("DOMContentLoaded", function () {
         });
 		
 	// handling navigation
-	const navbarLinks = document.querySelectorAll('.navbar-nav .nav-link');
-        const currentPath = window.location.pathname;
-        const currentPage = currentPath.substring(currentPath.lastIndexOf('/') + 1);
+	// const navbarLinks = document.querySelectorAll('.navbar-nav .nav-link');
+        // const currentPath = window.location.pathname;
+        // const currentPage = currentPath.substring(currentPath.lastIndexOf('/') + 1);
         
-        navbarLinks.forEach(link => {
-            const href = link.getAttribute('href');
+        // navbarLinks.forEach(link => {
+            // const href = link.getAttribute('href');
 
-            if (href.startsWith("#")) {
-                // Same page anchor
-                link.href = href;
-            } else if (currentPage === "pocetna.html") {
-                // On pocetna, remove "pocetna.html" from the path
-                link.href = href.replace("pocetna.html", "");
-            } else if (href.startsWith("pocetna")) {
-                // Keep "pocetna" if redirecting from another page
-                link.href = href;
-            } else {
-                // For other pages, prepend "pocetna" for section links
-                link.href = "pocetna" + link.getAttribute('href');
-            }
-	});
+            // if (href.startsWith("#")) {
+                // // Same page anchor
+                // link.href = href;
+            // } else if (currentPage === "pocetna.html") {
+                // // On pocetna, remove "pocetna.html" from the path
+                // link.href = href.replace("pocetna.html", "");
+            // } else if (href.startsWith("pocetna")) {
+                // // Keep "pocetna" if redirecting from another page
+                // link.href = href;
+            // } else {
+                // // For other pages, prepend "pocetna" for section links
+                // link.href = "pocetna" + link.getAttribute('href');
+            // }
+	// });
 });
